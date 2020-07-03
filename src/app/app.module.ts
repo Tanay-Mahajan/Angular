3940c1132a34fb92ffb3dbc761e0c8a4535ba008
Tanay-Mahajan <tanaymahajan0799@gmail.com>
2020-07-03 16:35:02 +0530

    Assignment 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,9 +21,11 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 
-import { DishService } from './services/dish.service';
 
+import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { LeaderService } from './services/leader.service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -48,7 +50,9 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     AppRoutingModule
   ],
   providers: [
-    DishService
+    DishService,
+    PromotionService,
+    LeaderService
   ],
   bootstrap: [AppComponent]
 })
