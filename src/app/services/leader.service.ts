38faf5b8e83268624a26c30ed4dc126ce54cdde5
Tanay-Mahajan <tanaymahajan0799@gmail.com>
2020-07-07 00:03:35 +0530

    loading

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -1,18 +1,31 @@
 import { Injectable } from '@angular/core';
 import { Leader } from '../shared/leader';
 import { LEADERS } from '../shared/leaders';
+import { promise } from 'protractor';
+import { resolve } from 'url';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LeaderService {
-  getLeaders(): Leader[] {
-    return LEADERS;
-  }
-  getFeaturedLeader(): Leader {
-    return LEADERS.filter((leader) => leader.featured)[0];
-  }
-
-
   constructor() { }
+
+  getLeaders(): Promise<Leader[]> {
+    return new Promise(resolve => {
+      setTimeout(() => resolve(LEADERS), 2000);
+   });
+  }
+
+  getLeader(id: string): Promise<Leader> {
+    return new Promise(resolve => {
+      setTimeout(() => resolve(LEADERS.filter((leader) => leader.id)[0]), 2000);
+   });
+  }
+
+  getFeaturedLeader():Promise<Leader> {
+    return new Promise(resolve => {
+      setTimeout(() => resolve(LEADERS.filter((leader) => leader.featured)[0]),2000);
+    });
+  }
+  
 }
